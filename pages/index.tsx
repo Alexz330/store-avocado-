9087795fd1daf229e3fc43e51092b1ec56cb4055
alexis <alexislc330@gmail.com>
2021-09-23 16:29:57 -0600

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import data from 'database/data';
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const HomePage = () => {
+
+
+  
+  return (
+    <div>
+      
+      <div>Platzi and Next.js!</div>
+     
+    </div>
+  )
+}
+
+export default HomePage
